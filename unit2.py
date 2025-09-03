b9bd5c9550d279b19f6aaa0da0307b425380ba25
AVANYA <avanyasangal29@gmail.com>
2025-09-03 09:36:52 +0530

    unit 2

---
+++ unit2.py
@@ -0,0 +1,23 @@
+#unit 2
+
+#taking input
+
+v=int(input("enter number: "))
+print (v)
+
+#if-else loop
+a=10
+b=5
+if a>b:        #bracket not needed here
+ print(a)
+elif a==b:
+ print(a)
+else:
+ print(b)
+ #short hand if_else
+c=5
+d=9
+
+
+print(c) if c>d else print(d)
+
